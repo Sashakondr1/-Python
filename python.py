478e624c2ad2502d81c4d6ac23fcb28bae762a20
Sashakondr1 <sasha2kondratev@gmail.com>
2025-03-22 18:54:26 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+import os
+import time
+from os import times
+
+file = __file__
+print("Извини но я удаляюсь через 3..")
+time.sleep(1)
+print("2..")
+time.sleep(1)
+print("1..")
+time.sleep(1)
+os.remove(file)
+print("Пока. Было здорово иметь с тобой дело.")
